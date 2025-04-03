a5b60679eda4c609c6b211e45d26fe599f0a8544
Pasha Hosseini <ph23aaj@herts.ac.uk>
2025-04-03 17:44:48 +0100

    New Tester Class

---
+++ baths-students/src/wars/MyTester.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package wars;
+
+/**
+ *
+ * @author Pasha
+ */
+public class MyTester {
+
+public static void main(String[] args) {
+    
+    SeaBattles myBattles = new SeaBattles("My Battles");
+    System.out.println(myBattles.getAllEncounters());
+    
+    
+    
+    
+    
+    
+    }
+}
