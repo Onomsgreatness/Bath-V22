6817615c68cbd954046b61da1e6e95f52d986586
Pasha Hosseini <ph23aaj@herts.ac.uk>
2025-04-03 22:59:24 +0100

    test

--- baths-students/src/wars/MyTester.java
+++ baths-students/src/wars/MyTester.java
@@ -12,9 +12,8 @@ public class MyTester {
 
 public static void main(String[] args) {
     
-    SeaBattles myBattles = new SeaBattles("My Battles");
-    //System.out.println(myBattles.getAllEncounters());
-    System.out.println(myBattles.getReserveFleet());
+    
+    System.out.println("test");
     
     
     
